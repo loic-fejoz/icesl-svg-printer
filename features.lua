e4afd2de849702d00865ec2e0a849c4bd4e9a0bd
Loïc Fejoz <loic@fejoz.net>
2015-04-23 10:04:23 +0200

    Only draw when extruding.
    Update to latest version of IceSL.
    Add layer_max configuration to easily print only the first layer.
    Use properly LineTo and MoveTo command to only draw when extruding.

--- features.lua
+++ features.lua
@@ -2,4 +2,4 @@ bed_size_x = 210
 bed_size_y = 297
 bed_size_z = 10
 z_offset   = 0.0
-nozzle_width = 0.35
+nozzle_width = -0.2
--- printer.lua
+++ printer.lua
@@ -1,4 +1,10 @@
--- Foldarap
+-- SVG printer
+
+layer_max = 1
+
+tool_e = 0
+tool_e_restart = 0
+layer_counter = 0
 
 function header()
   h = file('header.svg')
@@ -10,21 +16,37 @@ function footer()
 end
 
 function layer_start(zheight)
-   output('<g id="layer' .. f(zheight) .. '">')
-   output('<path style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" ')
-   output('d="M ')
+   layer_counter = layer_counter + 1
+   if (layer_counter <= layer_max) then
+      output('<g id="layer' .. f(zheight) .. '">')
+      output('<path style="fill:none;stroke:#000000;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" ')
+      output('d="')
+   end
 end
 
 function layer_stop()
-   output('" />')
-   output('</g>')  
+   if (layer_counter <= layer_max) then
+      output('" />')
+      output('</g>')
+   end
+end
+
+function retract(tool,e) 
+  len   = filament_priming_mm[tool]
+  speed = priming_mm_per_sec * 60;
+  letter = 'E'
+  return e - len
+end
+
+function prime(tool,e)
+  len   = filament_priming_mm[tool]
+  speed = priming_mm_per_sec * 60;
+  letter = 'E'
+  return e + len
 end
 
 current_tool = 0
 current_frate = 0
-
-tool_e = 0
-tool_e_restart = 0
 
 bed_center_x = bed_size_x/2
 bed_center_y = bed_size_y/2
@@ -35,21 +57,25 @@ end
 function swap_tool(from,to,x,y,z)
 end
 
+function move_xy_cmd(x,y,cmd)
+   if (layer_counter <= layer_max) then
+      output(cmd .. ' ' .. f(x+bed_center_x) .. ',' .. f(y+bed_center_y) .. ' ')
+   end
+end
+
 function move_xyz(x,y,z)
-   output(' ' .. f(x+bed_center_x) .. ',' .. f(y+bed_center_y) .. ' ')
+   move_xy_cmd(x,y,'M')
 end
 
 function move_xyze(x,y,z,e)
   tool_e = e
   letter = 'E'
-  --  output('G1 X' .. f(x+bed_center_x) .. ' Y' .. f(y+bed_center_y) .. ' Z' .. f(z+z_offset) .. ' F' .. current_frate .. ' ' .. letter .. f(e - tool_e_restart))
-  move_xyz(x,y,z)
+  move_xy_cmd(x,y,'L')
 end
 
 function move_e(e)
   tool_e = e
   letter = 'E'
---  output('G1 ' .. letter .. f(e - tool_e_restart))
 end
 
 function set_feedrate(feedrate)
